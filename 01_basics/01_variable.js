e495d11fa87e71d71477482a21a2ba7e5ebb4f23
Satish Prasad <118258417+Satishprasad018@users.noreply.github.com>
2024-01-02 13:49:54 +0000

    const let var console.table done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountID = 12321
+let accountEmail = "satish.prasad.018@gmail.com"
+var accountPassword = "12345" 
+accountCity = "pune"
+let accountState 
+
+//accountID = 3
+accountEmail = "bchjsbdvjsh"
+accountPassword = "bvbsjdh"
+accountCity = "idnuk"
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
