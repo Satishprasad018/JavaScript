2cdf8ea0c33d55b988836f920e7f561dc66a246d
Satish Prasad <118258417+Satishprasad018@users.noreply.github.com>
2024-01-03 03:38:52 +0000

    conversion basics

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,23 @@
+// let score = "33"
+// let score1 ="bvdskhj"
+// let score2 = null
+// let score3 = undefined
+
+// console.log(typeof(score));
+// console.log(typeof(score1));
+// console.log(typeof(score2));
+// console.log(typeof(score3));
+
+
+// let valueInNumber = Number(score)
+// let valueInNumber1 = Number(score1)
+// let valueInNumber2 = Number(score2)
+// let valueInNumber3 = Number(score3)
+
+
+// console.table([typeof(valueInNumber),typeof(valueInNumber1),typeof(valueInNumber2),typeof(valueInNumber3)]);
+// console.table([valueInNumber,valueInNumber1,valueInNumber2,valueInNumber3]);
+
+let boolean = "sarish" // "1"  1  ""
+let valueInBoolean = Boolean(boolean)
+console.log(valueInBoolean);
