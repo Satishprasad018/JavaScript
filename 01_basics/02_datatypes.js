0d0eaa1cd90aa05b6a07b869ae5093f969e53ce8
Satish Prasad <118258417+Satishprasad018@users.noreply.github.com>
2024-01-03 03:38:06 +0000

    basic datatype done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"user strict"; //treat all js codes as newr version
+
+// alert(3+3) //we are using node js, not browser
+
+let name = "satish";
+let age = 21;
+let isLoggedIn = false;
+
+// number => 2^53
+// BigInt =>
+// boolean => true/false
+// string
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "satish");
+console.log(typeof undefined);
+console.log(typeof null);
