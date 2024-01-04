57c45fbdfc809be9eafc10444416b498738c53a8
Satish Prasad <118258417+Satishprasad018@users.noreply.github.com>
2024-01-04 06:22:10 +0000

    comparison operator basics

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);   // convert null to 0  
+console.log(null == 0);  // == and ===  work differently
+console.log(null >= 0);  
+
+console.log(undefined == 0); // == and ===  work differently
+console.log(undefined > 0);  // convert null to 0
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+console.log("2" == "2");
