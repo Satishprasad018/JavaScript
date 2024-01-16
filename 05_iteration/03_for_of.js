ff85b458af38eb00fff581e641a5b6ef614ce43e
Satish Prasad <118258417+Satishprasad018@users.noreply.github.com>
2024-01-16 15:52:54 +0000

    for of loop in strings,array,objects

---
+++ 05_iteration/03_for_of.js
@@ -0,0 +1,29 @@
+// for of loop in array
+
+const arr1 = [1,2,3,4,5,6,7,8,9]          
+
+for (const i of arr1) {
+    console.log(`array element are ${i}`);
+}
+
+// fro of loop in strings
+const string1 = "satish prasad"
+
+for (const i of string1) {
+    // console.log(i);
+}
+
+// for of loop in objects ---------------->  objects is not iterable 
+
+const mySym = Symbol("key1")
+
+const object1 = {
+    name : "satish",
+    "lastName" : "prasad",
+    age : 19,
+    [mySym] : "password"
+}
+
+// for (const i of object1) {
+//     // console.log();       
+// }
