96f4787723c2cfb74ffd5bc780eff4b17ab49c9d
Satish Prasad <prasadsatish333@gmail.com>
2024-01-21 19:40:51 +0530

    restructured and revision 1

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,26 +1,27 @@
 
-const num1 = 400
-console.log(num1);
+const num1 = 46485.844
+console.log(num1);                       // 46485.844
 
 const num2 = new Number(46485.844)
-console.log(num2);
+console.log(num2);                      // [Number: 46485.844]
 
-console.log(typeof(num2.toString()));
+console.log(num2.toString());           // 46485.844
+console.log(typeof(num2.toString()));   // string
 
-console.log(num2.toFixed(3));            //
-console.log(num2.toPrecision(3));        //
+console.log(num2.toFixed(1));            // 46485.8
+console.log(num2.toPrecision(5));        // 46486
 
-console.log(num2.toLocaleString('en-IN'));
+console.log(num2.toLocaleString('en-IN')); // 46,485.844
 
 //+++++++++++++++++++++++++++++ Maths   +++++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
-console.log(Math.min(4, 3, 6, 8));
-console.log(Math.max(4, 3, 6, 8));
+console.log(Math);                      // Object [Math] {}
+console.log(Math.abs(-4));              // 4
+console.log(Math.round(4.6));           // 5
+console.log(Math.ceil(4.2));            // 5
+console.log(Math.floor(4.9));           // 5
+console.log(Math.min(4, 3, 6, 8));      // 3
+console.log(Math.max(4, 3, 6, 8));      // 8
 
 console.log(Math.random());     // value between 0 and 1 
 console.log(Math.random()*10); // if 0.706565416 -----> 7.06565416  exception 0.003672 ---> 0.03672     
