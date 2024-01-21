323a24539d2afe3008633abc0063a9a3ac644c30
Satish Prasad <prasadsatish333@gmail.com>
2024-01-21 19:45:52 +0530

    Updated and revision 1

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -8,7 +8,12 @@ console.log(num2);                      // [Number: 46485.844]
 console.log(num2.toString());           // 46485.844
 console.log(typeof(num2.toString()));   // string
 
+console.log(num2.toFixed());            // 46485
 console.log(num2.toFixed(1));            // 46485.8
+console.log(num2.toFixed(2));            // 46485.84
+console.log(num2.toPrecision());        // 46486.844
+console.log(num2.toPrecision(1));        // 5e+4
+console.log(num2.toPrecision(3));        // 465e+4
 console.log(num2.toPrecision(5));        // 46486
 
 console.log(num2.toLocaleString('en-IN')); // 46,485.844
@@ -23,9 +28,9 @@ console.log(Math.floor(4.9));           // 5
 console.log(Math.min(4, 3, 6, 8));      // 3
 console.log(Math.max(4, 3, 6, 8));      // 8
 
-console.log(Math.random());     // value between 0 and 1 
-console.log(Math.random()*10); // if 0.706565416 -----> 7.06565416  exception 0.003672 ---> 0.03672     
-console.log((Math.random()*10)+1); // exception cleared   0.003672 ---> 0.03672 + 1 ----> 1.003672
+console.log(Math.random());             // value between 0 and 1 
+console.log(Math.random()*10);          // if 0.706565416 -----> 7.06565416  exception 0.003672 ---> 0.03672     
+console.log((Math.random()*10)+1);     // exception cleared   0.003672 ---> 0.03672 + 1 ----> 1.003672
 
 //if range is given
 
