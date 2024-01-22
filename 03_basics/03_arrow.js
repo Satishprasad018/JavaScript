e4f5597654ce559b14742930093cb5e9d5045223
Satish Prasad <prasadsatish333@gmail.com>
2024-01-22 13:27:53 +0530

    restructure and revision 1

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -5,46 +5,46 @@ const user = {
     price: 99,
 
     welcomeMessage: function () {
-        console.log(`${this.username} has logged in `);    // satish has logged in
-        console.log(this);                                 //global object show karta hai in this ex. user object 
+        console.log(`${this.username} has logged in `);       
+        console.log(this);                  //global object show karta hai in this ex. user object show kar raha 
     }
 }
 
-// user.welcomeMessage();
-// user.username = "prasad";
-// user.welcomeMessage();
+user.welcomeMessage();                     // satish has logged in
+user.username = "prasad";
+user.welcomeMessage();                     // prasad has logged in
 
-// console.log(this);    // {}  in editor
-                      // global object i.e window in browser
+// console.log(this);                      // {}  in editor
+                                           // global object i.e window in browser
 
 //+++++++++++++++++++++++++++++++++ this IN NORMAL FUNCTION +++++++++++++++++++++++++++++++++++++++++++++++++
 
 function chai(){
     let username = " satish";
-    console.log(this) 
-    // console.log(this.username)  // not valid inside function
+    console.log(this)  
+    // console.log(this.username)           // not valid inside function
 }                    
 
-// chai()    // global object show karta hai in editor and window in browser
+// chai()                                  // global object show karta hai in editor and window in browser
 
 //+++++++++++++++++++++++++++++++++ ARROW FUNCTION +++++++++++++++++++++++++++++++++++++++++++++++++
 
-const chai1 = () => {         // arrow function
+const chai1 = () => {                            // arrow function
     let username = "hitesh";
     console.log(this);
 }
 
-// chai1()   // {}  in editor
-        // global object i.e window in browser
+// chai1()                                         // {}  in editor
+                                                   // global object i.e window in browser
 
-        //+++++++++++++++++++++++++++++++++ IMPLICIT RETURN +++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++ IMPLICIT RETURN +++++++++++++++++++++++++++++++++++++++++++++++++
    
-// const addTwo = (num1 , num2) => {
-//     return num1+num2 ;
-// }
+const addTwo_1 = (num1 , num2) => {
+    return num1+num2 ;
+}
 
-// const addTwo = (num1 ,num2) => (num1 + num2)  // IMPLICIT RETURN
+const addTwo_2 = (num1 ,num2) => (num1 + num2)     // IMPLICIT RETURN
 
-// const addTwo = (num1 ,num2) => ({username : "hitesh"}) 
+const addTwo_3 = (num1 ,num2) => ({username : "hitesh"}) 
 
-console.log(addTwo(5,4)) ;
+console.log(addTwo_1(5,4)) ;
